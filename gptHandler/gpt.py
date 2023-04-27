daaf58068de0d8055a6f51338dc147cd08d3f9f7
alextitonis <45359358+alextitonis@users.noreply.github.com>
2023-04-27 18:10:49 +0300

    Update gpt.py

--- gptHandler/gpt.py
+++ gptHandler/gpt.py
@@ -20,6 +20,28 @@ Example Output: forward, left, right, stop, forward
 Task: {task}
 Output:"""
 
+example = """### Information ###
+You are in a location that is Room. You are a navigation robot.
+Around you there are:
+* A Desk with a laptop
+* A Chair
+* A Window
+* Walls
+You can see: A Window in front of you.
+### Actions ###
+You can:
+Function - Command Name
+Move forward - forward
+Move backward - backward
+Move left - left
+Move right - right
+Stop - stop
+Break - break
+### Output ###
+Example Output: forward, left, right, stop, forward
+Task: Go to the window
+Output:"""
+
 bot = ChatBot("")
 
 prompt = basePrompt.format(
